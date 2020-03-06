4215d3375a5d6529cec5c093d7cfc348873d36a1
MightyChipmunk <35093963+MightyChipmunk@users.noreply.github.com>
2020-03-06 23:53:58 +0900

    Add files via upload

--- struct.cpp
+++ struct.cpp
@@ -4,12 +4,17 @@ struct student {
 	int age;
 	double height;
 	int weight;
-};
+}; //구조체 선언
+
+struct employee {
+	student emp;
+	int money;
+}; //중첩 구조체
 
 student func(int a, int b, int c) {
 	student tmp = { a, b, c };
 	return tmp;
-}
+} //구조체를 반환하는 함수
 
 int main(void) { 
 	int test;
@@ -19,6 +24,9 @@ int main(void) {
 
 	student st2 = func(21, 191.1, 98);
 	std::cout << st2.age << ' ' << st2.height << ' ' << st2.weight << ' ' << std::endl;
+
+	employee em1 = { st2, 100 };
+	std::cout << em1.emp.age << ' ' << em1.money << std::endl;
 
 	std::cin >> test;
 	return 0; 
